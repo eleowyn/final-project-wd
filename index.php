466f3d48a4e6ec464bc3cd490da62ac538b94b9c
elshera dahlan <elshera.dahlan@gmail.com>
2024-11-17 13:47:15 +0800

    changing css files

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $signupError = $_GET['signup_error'] ?? '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form - UNKLAB Lost & Found</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
